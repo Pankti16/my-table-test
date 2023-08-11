e0aa780a940bc22648de481f791c5e92e9c870e7
Pankti Shah <shahpankti98@gmail.com>
2023-08-11 16:26:12 +0800

    add change theme demo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import {Table} from "@Pankti16/my-table";
 
 function App() {
+  const tableRef = React.useRef();
   return (
-    <div className="App">
+    <div>
+    <div className="my-button-group">
+      <button className="my-button" onClick={() => {
+        tableRef?.current?.setCustomTheme(true);
+      }}>Change me</button>
+      <button className="my-button" onClick={() => {
+        tableRef?.current?.setCustomTheme();
+      }}>Change back</button>
+      </div>
       <Table
+        ref={tableRef}
         dataSource={[
           {
             id: 1,
@@ -21,7 +31,24 @@ function App() {
           },
         ]}
         sortableColumns={-1}
-        isSelect
+        sortableExcept={[0]}
+        isSelect={true}
+        isMultiSelect={false}
+        customTheme={{
+          "primary-color": 'orange',
+          "hover-color": 'gold',
+          "secondary-color": 'lightyellow',
+          "grey-color": "lightgoldenrodyellow",
+          "divider-color": "darkgoldenrod"
+        }}
+        primaryColumn={0}
+        // customTableWrapperStyle={{backgroundColor: 'orange'}} //correct
+        // customMoTableWrapperStyle={{backgroundColor: 'blanchedalmond'}} //correct
+        // customTableWrapperStyle={{myColor: 'blue'}} //in-correct
+        // customTableWrapperStyle="my-table-wrapper" //correct
+        // customHeaderStyle={{backgroundColor: 'orange'}} //correct
+        // customRowStyle={{backgroundColor: 'blanchedalmond'}} //correct
+        mobileHeader={'My Table'}
         onSelection={(vals) => {
           console.log(vals);
         }}
